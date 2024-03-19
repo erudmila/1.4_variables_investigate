5a3639a95cf74385fbe01fc50b1d18f074a3fb5f
erudmila <144694341+erudmila@users.noreply.github.com>
2024-03-19 13:16:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+### Example Code 1
+
+fName = "Mr"
+lName = "Colley"
+
+print(fName)
+
+# Identify a variable in the code
+ # Answer: fName and lName
+
+# Identify a string in the code
+ # Answer: "colley and "mr"
+
+# Line 4 is changed to lName = "Thorpe".  How does this affect the output?
+ #Answer= the output would not change since the print line only has the firs names in.
+# but it would be change to Thorpe instead of colley.
+
+# Line 3 is changed to fName = "Mrs". How does this affect the output?
+ # Answer= the output would have "mrs" instead of "ms"
+
+
+### Example Code 2
+
+num1 = 20
+num2 = 5
+
+total1 = num1 + 15
+total2 = num2 * 2
+total3 = num1 - num2
+
+print(total3)
+
+# What will be output by the program?
+ # Answer 15
+
+
+### Example Code 3
+
+name1 = "Ross" 
+name2 = "Monica" 
+name3 = "Joey" 
+name4 = "Rachel" 
+name5 = "Chandler" 
+ 
+print(name1 + " and " + name4) 
+print(name3) 
+ 
+name3 = "Phoebe" 
+
+
+# How many variables are used in the program?
+ # Answer 5
+
+# What would be the impact of changing  print(name1, " and ", name4) to print(name1, 
+#" and ", name5) ?
+ # Answer istead of ross and rachel it would be ross and chandler 
+
+# What is the purpose of the '+' symbol in  print(name1, " and ", name4) 
+ # Answer to connect those two variables together in a sentence for the computer to 
+# understand
+
+# The line  print(name3) is added to the end of the code.  Explain what it will do.
+ # Answer the program runs top to bottom so the one that is the latest it will use that
